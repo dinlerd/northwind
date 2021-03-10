8449daeac95d8cc06ac27099f90200d47d422a95
Deniz Dinler <dinlerd@hotmail.com>
2021-03-11 01:06:21 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
+//kendi projemizdeki default ve sonradan eklediklerimizi declarations kısmına koyuyoruz,
+//dışardan bizim yazmadığımız modulleri de import lara koyuyoruz
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,17 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
   //{} süslü parantez obje demek
-  products:Product[] = [ ];
+  products: Product[] = [];
+  dataLoaded = false;
 
-  constructor() {}
+  // productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // }; response.data yazdığımız için buna gerek kalmadı ProductResponseModel'e maplemek için
 
-  ngOnInit(): void {}
+  constructor(private productService:ProductService) {} //productcomponent product service'i kullanabilir demek
+  //constructor içine girilen değişken this ile bu class da ulaşılabilir, fakat c# ta bu mümkün değil
+
+  ngOnInit(): void {
+    //console.log('init çalıştı');
+    //bir fonksiyonun dışındaki bir şeye ulaşmak istediğinde this kulanıyoruz
+    this.getProducts();
+  }
+
+  //public void NgOnInit(){}
+  getProducts() {
+    //console.log("Api request başladı");
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded = true;
+      //console.log("Api request bitti");
+      //asenkron çalışır: aşağıdaki metod biti, bunun bitmesini beklemiyor
+    })
+    //console.log("Metod bitti");
+  }
 }
