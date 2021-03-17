27ec68b11c44c544354dd58663162dd9d233d3af
Deniz Dinler <dinlerd@hotmail.com>
2021-03-18 00:13:35 +0300

    cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms"
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
+
+import { ToastrModule } from "ngx-toastr";
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component'
 
 //kendi projemizdeki default ve sonradan eklediklerimizi declarations kısmına koyuyoruz,
 //dışardan bizim yazmadığımız modulleri de import lara koyuyoruz
@@ -15,12 +22,20 @@ import { NaviComponent } from './components/navi/navi.component';
     AppComponent,
     ProductComponent,
     CategoryComponent,
-    NaviComponent
+    NaviComponent,
+    VatAddedPipe,
+    FilterPipePipe,
+    CartSummaryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,6 +14,7 @@ export class ProductComponent implements OnInit {
   //{} süslü parantez obje demek
   products: Product[] = [];
   dataLoaded = false;
+  filterText="";
 
   // productResponseModel:ProductResponseModel={
   //   data:this.products,
@@ -19,10 +22,10 @@ export class ProductComponent implements OnInit {
   //   success:true
   // }; response.data yazdığımız için buna gerek kalmadı ProductResponseModel'e maplemek için
 
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute) {} 
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private toastrService:ToastrService,private cartService:CartService) {} 
   //productcomponent product service'i kullanabilir demek
   //constructor içine girilen değişken this ile bu class da ulaşılabilir, fakat c# ta bu mümkün değil
-
+  // activatedRoute aktifleştirilmiş mevcut route -> http://localhost:4200/cars/brand/1 gibi 
   ngOnInit(): void {
     //console.log('init çalıştı');
     //bir fonksiyonun dışındaki bir şeye ulaşmak istediğinde this kulanıyoruz
@@ -53,6 +56,15 @@ export class ProductComponent implements OnInit {
       this.dataLoaded = true;
 
     })
- 
+  }
+
+  addToCart(product: Product){
+    if(product.productId===1){
+      this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+    }else{
+      this.toastrService.success("Sepete eklendi",product.productName);
+      this.cartService.addToCart(product);
+    }
+    
   }
 }
